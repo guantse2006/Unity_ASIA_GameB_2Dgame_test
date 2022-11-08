cc21006abf81ee806b1d3126d63fc657ed913167
guantse2006 <114125776+guantse2006@users.noreply.github.com>
2022-11-09 03:17:07 +0800

    Unity 角色移動

---
+++ Unity_ASIA_GameB_2Dgame_test/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public Animator anim;
+    public float speed;
+    public float jumpforce;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        float horizontalmove = Input.GetAxis("Horizontal");
+        float facedircetion = Input.GetAxisRaw("Horizontal");
+
+        //角色移動
+        if (horizontalmove != 0)
+        {
+            rb.velocity = new Vector2(horizontalmove*speed * Time.deltaTime, rb.velocity.y);
+            anim.SetFloat("running", Mathf.Abs(facedircetion));
+        }
+        if(facedircetion != 0)
+        {
+            transform.localScale = new Vector3(facedircetion, 1, 1);
+        }
+
+        //角色跳躍
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpforce * Time.deltaTime);
+        }
+
+    }
+
+
+}
